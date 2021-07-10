bd39d9e0afa8f69141d5196da453e4b63f3ff6df
VincentVoigtlaender <80749062+VincentVoigtlaender@users.noreply.github.com>
2021-07-10 19:04:40 +0200

    Update main.cpp

--- Singleton_Papst/src/main.cpp
+++ Singleton_Papst/src/main.cpp
@@ -1,10 +1,23 @@
-/* main.cpp
+/*
+ * main.cpp
+ *
+ *  Created on: 09.07.2021
+ *      Author: mrt
+ */
 
-#include <iostream>
+#include "kardinal.h"
 
 
-int main()  {
+int main() {
 
-  return 0;
+	Kardinal kardinal_1;
+	Kardinal kardinal_2;
+
+
+	kardinal_1.papst_waehlen("Benedikt der XVI.");
+	kardinal_2.papst_waehlen("Franziskus");
+
+
+	return 0;
 
 }
