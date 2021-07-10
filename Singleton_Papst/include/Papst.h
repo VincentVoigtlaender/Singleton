b3669dd6cc1d86d535ef007a7673d58e3f2dfa88
VincentVoigtlaender <80749062+VincentVoigtlaender@users.noreply.github.com>
2021-07-10 19:06:26 +0200

    Update Papst.h

--- Singleton_Papst/include/Papst.h
+++ Singleton_Papst/include/Papst.h
@@ -1,4 +1,44 @@
-#ifndef KOENIGIN_H_
-#define KOENIGIN_H_
+/*
+ * papst.h
+ *
+ *  Created on: 09.07.2021
+ *      Author: mrt
+ */
 
-#endif /* KOENIGIN_H_ */
+#ifndef INCLUDE_PAPST_H_
+#define INCLUDE_PAPST_H_
+
+#include <string>
+#include <iostream>
+
+class Papst
+{
+
+private:
+	Papst(std::string wunschname); // privater Konstruktor
+	~Papst(); // privater Destruktor
+
+	// statischer Pointer auf das einzige Objekt
+	static Papst* papst_;
+
+	// Attribut, um Instanzen zu unterscheiden
+	std::string name;
+
+
+public:
+	// Kopier-Operator muss gelöscht werden
+	Papst(Papst const&) = delete;
+
+	// Zuweisung-Operator muss gelöscht werden
+	void operator=(Papst const&) = delete;
+
+	// Methode, um Pointer auf das einzige Objekt zu bekommen
+	static Papst* getPapst(std::string wunschname);
+
+	// Methode, um potentiell unterschiedliche Instanzen
+	//  unterscheiden zu können
+	std::string getName();
+
+};
+
+#endif /* INCLUDE_PAPST_H_ */
